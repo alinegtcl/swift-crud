6c860134cd81ca520890b20a98b79dfdf5ee3bdd
Aline Tolentino <linegcruz@gmail.com>
2024-10-31 22:22:07 -0300

    feat: initialize program with main menu

--- avaliacao-swift/main.swift
+++ avaliacao-swift/main.swift
@@ -7,5 +7,50 @@
 
 import Foundation
 
-print("Hello, World!")
+executarPrograma()
 
+func executarPrograma() {
+    var continuar = true
+    print("--CONTATOS--")
+    while continuar {
+        mainMenu()
+        
+        guard let opcao = readLine(), let escolha = Int(opcao) else {
+            print("Opção inválida.")
+            continue
+        }
+        
+        switch escolha {
+        case 1:
+            print("incluir")
+        case 2:
+            print("alterar")
+        case 3:
+            print("apagar")
+        case 4:
+            print("exibir uma pessoa")
+        case 5:
+            print("exibir todas as pessoas")
+        case 6:
+            continuar = false
+            print("Saindo do programa.")
+        default:
+            print("Opção inválida.")
+        }
+    }
+}
+
+func mainMenu() {
+    print("""
+    
+    -Opções do Menu-
+    1. Incluir Pessoa
+    2. Alterar Pessoa
+    3. Apagar Pessoa
+    4. Exibir Pessoa pelo Nome
+    5. Exibir todas as Pessoas
+    6. Sair
+    
+    Escolha uma opção:
+    """)
+}
